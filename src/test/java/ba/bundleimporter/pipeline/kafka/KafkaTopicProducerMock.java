569b15ca04686d99b65a219ed63c50c7b4304529
Alan <aklikic@gmail.com>
2022-01-31 19:27:49 +0100

    Fix for data type Bundle -> BundleOut

--- src/test/java/ba/bundleimporter/pipeline/kafka/KafkaTopicProducerMock.java
+++ src/test/java/ba/bundleimporter/pipeline/kafka/KafkaTopicProducerMock.java
@@ -5,30 +5,31 @@ import akka.actor.ActorSystem;
 import akka.kafka.ProducerMessage;
 import akka.stream.javadsl.Flow;
 import ba.bundleimporter.datamodel.Bundle;
+import ba.bundleimporter.datamodel.BundleOut;
 import ba.bundleimporter.kafka.KafkaTopicProducer;
 import ba.bundleimporter.pipeline.component.serialization.Serializer;
 
 import java.io.IOException;
 
-public class KafkaTopicProducerMock<C> extends KafkaTopicProducer<Bundle,C> {
+public class KafkaTopicProducerMock<C> extends KafkaTopicProducer<BundleOut,C> {
 
     public KafkaTopicProducerMock(ActorSystem system, String topicName) {
         super(system, topicName,key->null);
     }
 
     @Override
-    protected Flow<ProducerMessage.Envelope<String, byte[], Bundle>, Bundle, NotUsed> producerFlow() {
-        return Flow.<ProducerMessage.Envelope<String, byte[], Bundle>>create()
+    protected Flow<ProducerMessage.Envelope<String, byte[], BundleOut>, BundleOut, NotUsed> producerFlow() {
+        return Flow.<ProducerMessage.Envelope<String, byte[], BundleOut>>create()
                 .map(message->message.passThrough());
     }
 
     @Override
-    protected byte[] serialize(Bundle input) throws IOException {
-        return Serializer.serializeBundle(input);
+    protected byte[] serialize(BundleOut input) throws IOException {
+        return Serializer.serializeBundleOut(input);
     }
 
     @Override
-    protected String extractKey(Bundle input) {
+    protected String extractKey(BundleOut input) {
         return input.getBundleId().toString();
     }
 
